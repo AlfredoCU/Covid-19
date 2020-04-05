d4743cc449e68c628101dfd96e8b431697beb7d0
Alfredo_CU7557 <alfredo_11_cu@hotmail.com>
2020-04-04 20:41:15 -0600

    Iniciando el proyecto COVID-19

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>COVID-19</p>
+        <Button variant="outlined" color="secondary">
+          Trabajando
+        </Button>
       </header>
     </div>
   );
