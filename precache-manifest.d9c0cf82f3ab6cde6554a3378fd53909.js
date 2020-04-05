47f491f8d623d78d3969369688eca66c386f1410
Alfredo_CU7557 <alfredo_11_cu@hotmail.com>
2020-04-04 20:43:32 -0600

    Updates

---
+++ precache-manifest.d9c0cf82f3ab6cde6554a3378fd53909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78112c9c77470624cede42a4b49947b6",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "5fa40d491b1cbdbc46de",
+    "url": "/Covid-19/static/css/main.c5025772.chunk.css"
+  },
+  {
+    "revision": "da29fa374407868bb61c",
+    "url": "/Covid-19/static/js/2.b3796c01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Covid-19/static/js/2.b3796c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa40d491b1cbdbc46de",
+    "url": "/Covid-19/static/js/main.6eec6f0b.chunk.js"
+  },
+  {
+    "revision": "6c0b24b2bb53c35f7552",
+    "url": "/Covid-19/static/js/runtime-main.6aac1f26.js"
+  }
+]);
